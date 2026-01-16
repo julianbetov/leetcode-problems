d694d408b291bef9b653fb3ea6439290853d0dc6
Julian Bermudez <julianbetov@gmail.com>
2026-01-16 11:41:43 -0500

    Add solution for LeetCode easy challenge: Valid Anagram

---
+++ src/practice/leetcode/easy/_242ValidAnagram.java
@@ -0,0 +1,11 @@
+package practice.leetcode.easy;
+
+public class _242ValidAnagram {
+    private static boolean isAnagram(String s, String t) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram")); // true
+    }
+}
