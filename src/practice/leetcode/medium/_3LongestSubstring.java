76483a6a00c2cda47c220ad3fb44d628a6a1b1c9
Julian Bermudez <julianbetov@gmail.com>
2026-01-16 20:07:22 -0500

    Add solution for LeetCode medium challenge: Longest Substring Without Repeating Characters

---
+++ src/practice/leetcode/medium/_3LongestSubstring.java
@@ -0,0 +1,11 @@
+package practice.leetcode.medium;
+
+public class _3LongestSubstring {
+    private static int lengthOfLongestSubstring(String s) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+    }
+}
