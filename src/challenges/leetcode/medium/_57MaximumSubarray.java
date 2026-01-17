3381c445004ac678a45ee4dec8f8481bf56d1f25
Julian Bermudez <julianbetov@gmail.com>
2026-01-16 20:11:06 -0500

    Add solution for LeetCode medium challenge: Maximum Subarray

---
+++ src/challenges/leetcode/medium/_57MaximumSubarray.java
@@ -0,0 +1,18 @@
+package challenges.leetcode.medium;
+
+public class _57MaximumSubarray {
+    private static int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], nums[i] + currentSum);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums)); // 6
+    }
+}
---
+++ src/practice/leetcode/medium/_57MaximumSubArray.java
@@ -0,0 +1,12 @@
+package practice.leetcode.medium;
+
+public class _57MaximumSubArray {
+    private static int maxSubArray(int[] nums) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums)); // 6
+    }
+}
